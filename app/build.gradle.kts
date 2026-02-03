e7b0c1d384664b4194c1bc055d7cc6bd7c70c4e9
LOVECARSTONE\kdc62 <kdc6208@naver.com>
2026-02-03 15:49:03 +0900

    chore: Setup project dependencies with Version Catalog
    - Configure Hilt and Kapt for dependency injection
    - Add Navigation Compose and Material Icons Extended
    - Migrate dependency management to Version Catalog (libs.versions.toml)
    - Register MarketApplication Hilt entry point in Manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("kotlin-kapt")
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -56,4 +59,12 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    
+    implementation(libs.androidx.material.icons.extended)
+
 }
